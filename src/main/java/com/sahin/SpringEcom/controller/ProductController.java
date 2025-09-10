7ea274c6f57c4fba19ed7a6e1d957f23695c5c90
sahin Islam <sahinislam878@gmail.com>
2025-09-10 16:22:00 +0530

    Progress: Completed ResponseEntity From Udemy

--- src/main/java/com/sahin/SpringEcom/controller/ProductController.java
+++ src/main/java/com/sahin/SpringEcom/controller/ProductController.java
@@ -3,10 +3,9 @@ package com.sahin.SpringEcom.controller;
 import com.sahin.SpringEcom.model.Product;
 import com.sahin.SpringEcom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +18,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts(){
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.ACCEPTED);
     }
 
 }
