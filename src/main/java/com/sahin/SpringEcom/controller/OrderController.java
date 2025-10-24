e380b9b6719f111606b8b6fbf0335db8df302062
sahin Islam <sahinislam878@gmail.com>
2025-10-24 16:09:14 +0530

    Progress: Completed Creating Order Controller From Udemy

---
+++ src/main/java/com/sahin/SpringEcom/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.sahin.SpringEcom.controller;
+
+import com.sahin.SpringEcom.model.dto.OrderRequest;
+import com.sahin.SpringEcom.model.dto.OrderResponse;
+import com.sahin.SpringEcom.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+
+    @PostMapping("/orders/place")
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
+        OrderResponse orderResponse = OrderService.placeOrder(orderRequest);
+        return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderResponse>> getAllOrders(){
+        List<OrderResponse> responses = orderService.getAllOrderResponses();
+        return new ResponseEntity<>(responses,HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/sahin/SpringEcom/service/OrderService.java
@@ -0,0 +1,21 @@
+package com.sahin.SpringEcom.service;
+
+import com.sahin.SpringEcom.model.dto.OrderRequest;
+import com.sahin.SpringEcom.model.dto.OrderResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    public static OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+}
